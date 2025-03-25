e26f0b8aeb786cd5f80c8780f20f0b2553515118
unbreakk1 <95477818+unbreakk1@users.noreply.github.com>
2025-03-25 14:12:54 +0100

    fixed nothing i guess oO

--- backend/src/test/java/org/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/org/example/backend/BackendApplicationTests.java
@@ -7,7 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 class BackendApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads()
+	{
+		System.out.println("Hello World");
 	}
 
 }
